f1683eab77171455bd7ce07f773521feadff6f0f
AfraidOfCommits <alexandre.lecuyer.1@umontreal.ca>
2023-12-06 13:53:13 -0500

    Bug fixes after testing with other sequences

--- plast.py
+++ plast.py
@@ -15,10 +15,10 @@ import argparse
 # LECTURE DU FICHIER
 def read_fasta(chemin_fichier):
     """ Utilise SeqIO de Biopython pour lire un fichier FASTA et construire la base de données/dictionnaire. """
-    base_de_donnees = {}
+    database = {}
     for read in SeqIO.parse(chemin_fichier, "fasta"):
-        base_de_donnees[read.id] = str(read.seq)
-    return base_de_donnees
+        database[read.id] = str(read.seq)
+    return database
 
 
 # ---------------------------------------------------------------------------------------
@@ -30,10 +30,10 @@ def construire_kmers(sequence, k):
     return [sequence[i:i + k] for i in range(len(sequence) - k + 1)]
 
 
-def recherche_exacte_kmers(kmers, base_de_donnees):
+def recherche_exacte_kmers(kmers, database):
     """ Recherche chaque k-mer dans la base de données et renvoie les HSPs initiaux avec les positions dans sequence. """
     HSPs = []
-    for seq_id, db_seq in base_de_donnees.items():
+    for seq_id, db_seq in database.items():
         for kmer in kmers:
             start_pos_db = db_seq.find(kmer)
             while start_pos_db != -1:
@@ -49,47 +49,50 @@ def recherche_exacte_kmers(kmers, base_de_donnees):
 # Il faut ensuite étendre la similitude dans les deux sens (en ignorant la possibilité de gaps) le long
 # de chaque séquence, à partir du HSP initial, de manière à ce que le score cumulé puisse être amélioré.
 # Seule l'extension produisant le meilleur score pour le HSP étendu sera choisi.
-def extend_hsp(sequence, hsp, base_de_donnees, score_match=5, score_mismatch=-4, seuil_extension=4):
-    seq_id, start_db_seq, end_db_seq, kmer = hsp
-    db_seq = base_de_donnees[seq_id]
+def extend_hsp(sequence, hsp, database, score_match=5, score_mismatch=-4, seuil_extension=4):
+    seq_id, db_left, db_right, kmer = hsp
+    db_seq = database[seq_id]
 
     # Initialisez le meilleur score au score de l'overlap initial.
-    score_total = score_max = (end_db_seq - start_db_seq + 1) * score_match
-    ext_gauche, ext_droite = start_db_seq, end_db_seq
-    start_sequence = sequence.find(kmer)
-    end_sequence = start_sequence + len(kmer) - 1
-    best_extension = (seq_id, ext_gauche, ext_droite, start_sequence, end_sequence, score_max)
+    score_total = score_max = (db_right - db_left + 1) * score_match
+    sq_left = sequence.find(kmer)
+    sq_right = sq_left + len(kmer) - 1
+    best_extension = (seq_id, db_left, db_right, sq_left, sq_right, score_max)
 
     while True:
-        score_gauche, score_droite = 0, 0
+        score_left, score_right = 0, 0
 
         # Vérifiez l'extension à gauche
-        if (ext_gauche > 0) and (start_sequence > 0):
-            score_gauche = score_match if sequence[start_sequence - 1] == db_seq[ext_gauche - 1] else score_mismatch
+        if (db_left > 0) and (sq_left > 0):
+            score_left = score_match if sequence[sq_left - 1] == db_seq[db_left - 1] else score_mismatch
 
         # Vérifiez l'extension à droite
-        if (ext_droite < len(db_seq) - 1) and (end_sequence < len(sequence) + 1):
-            score_droite = score_match if sequence[end_sequence + 1] == db_seq[ext_droite + 1] else score_mismatch
+        if (db_right < len(db_seq) - 1) and (sq_right < len(sequence) + 1):
+            score_right = score_match if sequence[sq_right + 1] == db_seq[db_right + 1] else score_mismatch
 
         # Arrêter si aucune extension n'est possible
-        if score_gauche == 0 and score_droite == 0:
+        if score_left == 0 and score_right == 0:
             break
 
         # Choisir la meilleure extension
-        if (score_gauche >= score_droite):
-            ext_gauche -= 1
-            start_sequence -= 1
-            score_total += score_gauche
+        if (score_left != 0) and (score_left >= score_right):
+            db_left -= 1
+            sq_left -= 1
+            score_total += score_left
 
-        elif score_droite > 0:
-            ext_droite += 1
-            end_sequence -= 1
-            score_total += score_droite
+        elif (score_right != 0):
+            db_right += 1
+            sq_right += 1
+            score_total += score_right
+
+        else:
+            return best_extension
+
 
         # Mettre à jour le meilleur score
         if score_total > score_max:
             score_max = score_total
-            best_extension = (seq_id, ext_gauche, ext_droite, start_sequence, end_sequence, score_max)
+            best_extension = (seq_id, db_left, db_right, sq_left, sq_right, score_max)
 
         # Si le score total baisse trop, revenir à la meilleure extension et arrêter
         elif score_max - score_total >= seuil_extension:
@@ -102,8 +105,11 @@ def extend_hsp(sequence, hsp, base_de_donnees, score_match=5, score_mismatch=-4,
 # ---------------------------------------------------------------------------------------
 # MERGE ALIGNED HSP MATCHES
 # Fusionner tous les HSPs se chevauchant et impliquant les mêmes séquences
-def fusionner_hsp(hsps):
+def fusionner_hsp(hsps, database, sequence):
     """ Fusionne les HSPs chevauchants. """
+    if(len(hsps) == 0):
+        return None
+
     # Tri des HSPs par identifiant de séquence et position de début
     hsps.sort(key=lambda x: (x[0], x[1]))
     fusionnes = []
@@ -114,9 +120,9 @@ def fusionner_hsp(hsps):
         # Vérifier si le HSP actuel et le HSP courant se chevauchent et concernent la même séquence
         if hsp[0] == hsp_courant[0] and hsp[1] <= hsp_courant[2]:
             # Si les HSPs se chevauchent, mettre à jour la fin du HSP courant si nécessaire
-            hsp_courant = (hsp_courant[0], hsp_courant[1], max(hsp_courant[2], hsp[2]), hsp_courant[5])
+            hsp_courant = (hsp_courant[0], hsp_courant[1], max(hsp_courant[2], hsp[2]), hsp_courant[3], max(hsp_courant[4], hsp[4]), hsp_courant[5])
         else:
-            # Si les HSPs ne se chevauchent pas, ajouter le HSP courant à la liste des fusionnés
+            # Si les HSPs ne se chevauchent pas, ajouter le HSP courant à la liste des fusionnés, puis passer au prochain
             fusionnes.append(hsp_courant)
             hsp_courant = hsp
 
@@ -145,12 +151,13 @@ def calculer_e(m, n, bitscore):
 def filtrer_hsp_significatifs(hsps, size_totale_reads, size_of_sequence_to_lookup, seuil):
     """ Filtre les HSPs en fonction de leur e-value. """
     hsp_significatifs = []
-    for hsp in hsps:
-        bitscore = calculer_bitscore(hsp[5], lambda_value=0.192, K=0.176)
-        e_value = calculer_e(size_totale_reads, size_of_sequence_to_lookup, bitscore)
+    if(hsps != None and len(hsps) > 0):
+        for hsp in hsps:
+            bitscore = calculer_bitscore(hsp[5], lambda_value=0.192, K=0.176)
+            e_value = calculer_e(size_totale_reads, size_of_sequence_to_lookup, bitscore)
 
-        if e_value < seuil:
-            hsp_significatifs.append(hsp + (e_value,))
+            if e_value < seuil:
+                hsp_significatifs.append(hsp + (e_value,))
     return hsp_significatifs
 
 
@@ -174,14 +181,14 @@ if __name__ == "__main__":
     # La taille des k-mers est la longueur du motif de la graine
     k_size = len(seed_pattern)  # Vous pouvez ajuster la taille des k-mers ici
 
-    base_de_donnees = read_fasta(chemin_fichier)
+    database = read_fasta(chemin_fichier)
     kmers = construire_kmers(sequence_to_search, k_size)
-    HSPs_initiaux = recherche_exacte_kmers(kmers, base_de_donnees)
-    HSPs_etendus = [extend_hsp(sequence_to_search, hsp, base_de_donnees, score_match=5, score_mismatch=-4, seuil_extension=seuil_extension)  for hsp in HSPs_initiaux]
-    HSPs_fusionnes = fusionner_hsp(HSPs_etendus)
+    HSPs_initiaux = recherche_exacte_kmers(kmers, database)
+    HSPs_etendus = [extend_hsp(sequence_to_search, hsp, database, score_match=5, score_mismatch=-4, seuil_extension=seuil_extension)  for hsp in HSPs_initiaux]
+    HSPs_fusionnes = fusionner_hsp(HSPs_etendus, database, sequence_to_search)
 
     # Trier les HSPs par e-value croissante
-    HSPs_significatifs = filtrer_hsp_significatifs(HSPs_fusionnes, len(base_de_donnees), len(sequence_to_search), seuil_e_value)
+    HSPs_significatifs = filtrer_hsp_significatifs(HSPs_fusionnes, len(database), len(sequence_to_search), seuil_e_value)
     HSPs_significatifs.sort(key=lambda x: x[-1])
 
     # Sélectionner le meilleur HSP pour chaque séquence
@@ -197,7 +204,7 @@ if __name__ == "__main__":
         print(f"# Best HSP score: {hsp[5]}, bitscore: {calculer_bitscore(hsp[5], 0.192, 0.176)}, evalue: {hsp[-1]:.2e}")
 
         start, end, start_seq, end_seq = hsp[1], hsp[2], hsp[3], hsp[4]
-        print(f"{start} {base_de_donnees[seq_id][start:end + 1]} {end}")
+        print(f"{start} {database[seq_id][start:end + 1]} {end}")
         print(f"{start_seq} {sequence_to_search[start_seq:end_seq + 1]} {end_seq}\n")
 
     print("--------------------------------------------------")
